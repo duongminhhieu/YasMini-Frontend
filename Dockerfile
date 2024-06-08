aef611704efeef333c45ba8df96c20f34953442b
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-06-09 00:11:47 +0700

    cicd: update entrypoint.sh

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,11 @@ COPY package.json package-lock.json ./
 RUN npm install
 
 COPY . .
+
+# Pass the environment variable to the build
+ARG VITE_BACKEND_URL
+ENV VITE_BACKEND_URL=$REACT_APP_BACKEND_URL
+
 RUN npm run build
 
 # Stage 2: Serve the application with Nginx
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
-# Replace environment variables in JavaScript files
-for file in /usr/share/nginx/html/assets/*.js; do
-  sed -i "s|VITE_BACKEND_URL_PLACEHOLDER|$VITE_BACKEND_URL|g" $file
-done
+# Generate .env file
+echo "VITE_BACKEND_URL=${VITE_BACKEND_URL}" > /app/.env
 
-exec "$@"
+# Start nginx
+nginx -g 'daemon off;'
