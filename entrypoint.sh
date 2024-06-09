54c397b80db08e4b35382b730b9c0d09c29e79f7
duongminhhieu <duongminhhieu2082002@gmail.com>
2024-06-09 13:40:09 +0700

    cicd: test enviroment variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,9 +5,9 @@
 
 
 # Replace environment variables in JavaScript files
-for file in /usr/share/nginx/html/assets/*.js; do
-  sed -i "s|VITE_BACKEND_URL_PLACEHOLDER|$VITE_BACKEND_URL|g" $file
-done
+# for file in /usr/share/nginx/html/assets/*.js; do
+#   sed -i "s|VITE_BACKEND_URL_PLACEHOLDER|$VITE_BACKEND_URL|g" $file
+# done
 
 # Start nginx
 nginx -g 'daemon off;'
